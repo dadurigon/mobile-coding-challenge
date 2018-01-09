f8ae8d6fad559de9bc29a308d404408ca34f6af4
Dion Durigon <dadurigon@icloud.com>
2018-01-09 00:28:08 -0500

    Updated ViewController with CollectionView and switching between Grid and Carousel layouts

--- UnsplashDemo/ViewController.swift
+++ UnsplashDemo/ViewController.swift
@@ -8,11 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private let reuseIdentifier = "cell"
 
+class ViewController: UIViewController  {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+ 
+    var gridLayout: GridLayout!
+    lazy var carouselLayout: CarouselLayout = {
+        var carouselLayout = CarouselLayout(itemHeight: 180)
+        return carouselLayout
+    }()
+    
+    var photos: [Photo] = []
+    var page = 1
+    var currentIndex: IndexPath?
+    
+    func switchLayout() {
+        if collectionView.collectionViewLayout == gridLayout {
+            // carousel layout
+            self.collectionView.isPagingEnabled = true
+            self.collectionView.alwaysBounceVertical = false
+            self.collectionView.alwaysBounceHorizontal = true
+            UIView.animate(withDuration: 1.0, animations: {
+                self.collectionView.collectionViewLayout.invalidateLayout()
+                
+                self.collectionView.setCollectionViewLayout(self.carouselLayout, animated: true)
+            })
+        } else {
+            // grid layout
+            self.collectionView.isPagingEnabled = false
+            self.collectionView.alwaysBounceVertical = true
+            self.collectionView.alwaysBounceHorizontal = false
+            UIView.animate(withDuration: 1.0, animations: {
+                self.collectionView.collectionViewLayout.invalidateLayout()
+                
+                self.collectionView.setCollectionViewLayout(self.gridLayout, animated: true)
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Unsplash"
+        
+        loadData(page: 1)
+        
+        gridLayout = GridLayout(numberOfColumns: 3)
+        collectionView.collectionViewLayout = gridLayout
+        collectionView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +65,83 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func loadData(page: Int) {
+        UnspashClient().fetchCuratedPhotos(page: page) { photoArray in
+            if photoArray.count > 0 {
+                self.photos += photoArray
+                self.collectionView?.reloadData()
+            }
+        }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+ 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoCollectionViewCell
+        
+        let unsplash = self.photos[indexPath.row]
+        
+        if let photo = unsplash.photoImage {
+            cell.imageView.image = photo
+        } else if let url = unsplash.photoURL {
+            DispatchQueue.global().async {
+                if let photoData = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: photoData)
+                        self.photos[indexPath.row].photoImage = image
+                        cell.imageView.image = image
+                    }
+                }
+            }
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        switchLayout()
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        var shouldLoadMore = false
+        
+        if collectionView.collectionViewLayout == carouselLayout {
+            self.collectionView?.contentOffset.x = scrollView.contentOffset.x
+            
+            let offsetX = scrollView.contentOffset.x
+            let scrollWidth = scrollView.frame.size.width
+            let contentWidth = scrollView.contentSize.width
+            
+            if ((offsetX + scrollWidth) >= contentWidth) {
+                shouldLoadMore = true
+            }
+            
+        } else {
+            let offsetY = scrollView.contentOffset.y
+            let scrollHeight = scrollView.frame.size.height
+            let contentHeight = scrollView.contentSize.height
+            
+            if ((offsetY + scrollHeight) >= contentHeight) {
+                shouldLoadMore = true
+            }
+        }
+        
+        if shouldLoadMore {
+            self.page += 1
+            loadData(page: self.page)
+        }
+    }
+}
